3fbdccf3593f8f361996bf17e395a0be3339e5e4
A1abhiral <mr.abhiral.p@gmail.com>
2024-08-26 11:17:20 +0545

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import json
+
+import requests
+while True:
+    city = input("enter the name of the city")
+    if city == "exit":
+        print("thankyou for using")
+        break
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=ae3ed4fc4dd73616a06c3328fb89048b&units=metric"
+    r = requests.get(url)
+    dic = json.loads(r.text)
+    print(f"the weather of {city} is: {dic["main"]["temp"]}")
+
